507cdac807ebd36981b126b47527d6d41c563e86
chaosinthecrd <thomas.meadows@jetstack.io>
2023-02-13 16:52:07 +0000

    temporary change

--- scripts/install.sh
+++ scripts/install.sh
@@ -78,123 +78,123 @@ do
   export HOSTNAME=$host
 	break
 done
-
-info "Enter password for 'tom' user ..."
-mkdir -p /mnt/keep/etc/users
-mkdir -p /etc/users
-mkpasswd -m sha-512 | tr -d "\n\r" > /tmp/tom
-
-################################################################################
-
-export ZFS_POOL="rpool"
-
-# ephemeral datasets
-export ZFS_LOCAL="${ZFS_POOL}/local"
-export ZFS_DS_ROOT="${ZFS_LOCAL}/root"
-# The below are kept on this machine.
-export ZFS_DS_NIX="${ZFS_LOCAL}/nix"
-export ZFS_DS_KEEP="${ZFS_LOCAL}/keep"
-
-# Persistent datasets which are backed up.
-export ZFS_SAFE="${ZFS_POOL}/safe"
-export ZFS_DS_PERSIST="${ZFS_SAFE}/persist"
-
-export ZFS_BLANK_SNAPSHOT="${ZFS_DS_ROOT}@blank"
-
-################################################################################
-
-info "Running the UEFI (GPT) partitioning and formatting directions from the NixOS manual ..."
-parted "$DISK_PATH" -- mklabel gpt
-parted "$DISK_PATH" -- mkpart primary 512MiB 100%
-parted "$DISK_PATH" -- mkpart ESP fat32 1MiB 512MiB
-parted "$DISK_PATH" -- set 2 boot on
-export DISK_PART_ROOT="${DISK_PATH}${PARTITION_PREFIX}1"
-export DISK_PART_BOOT="${DISK_PATH}${PARTITION_PREFIX}2"
-
-info "Formatting boot partition ..."
-mkfs.fat -F 32 -n boot "$DISK_PART_BOOT"
-
-info "Creating '$ZFS_POOL' ZFS pool for '$DISK_PART_ROOT' ..."
-zpool create -f \
-  -o ashift=12 \
-  -o autotrim=on \
-  -O canmount=off \
-  -O mountpoint=none \
-  -O acltype=posixacl \
-  -O compression=lz4 \
-  -O dnodesize=auto \
-  -O relatime=on \
-  -O normalization=formD \
-  -O xattr=sa \
-  -O encryption=aes-256-gcm \
-  -O keylocation=prompt \
-  -O keyformat=passphrase "$ZFS_POOL" "$DISK_PART_ROOT"
-
-info "Creating '$ZFS_DS_ROOT' ZFS dataset ..."
-zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_ROOT"
-
-info "Creating '$ZFS_DS_NIX' ZFS dataset ..."
-zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_NIX"
-
-info "Disabling access time setting for '$ZFS_DS_NIX' ZFS dataset ..."
-zfs set atime=off "$ZFS_DS_NIX"
-
-info "Creating '$ZFS_DS_KEEP' ZFS dataset ..."
-zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_KEEP"
-
-info "Creating '$ZFS_DS_PERSIST' ZFS dataset ..."
-zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_PERSIST"
-
-info "Permit ZFS auto-snapshots on ${ZFS_SAFE}/* datasets ..."
-zfs set com.sun:auto-snapshot=true "$ZFS_DS_PERSIST"
-
-info "Creating '$ZFS_BLANK_SNAPSHOT' ZFS snapshot ..."
-zfs snapshot "$ZFS_BLANK_SNAPSHOT"
-
-info "Mounting '$ZFS_DS_ROOT' to /mnt ..."
-mount -t zfs "$ZFS_DS_ROOT" /mnt
-
-info "Mounting '$DISK_PART_BOOT' to /mnt/boot ..."
-mkdir /mnt/boot
-mount -t vfat "$DISK_PART_BOOT" /mnt/boot
-
-info "Mounting '$ZFS_DS_NIX' to /mnt/nix ..."
-mkdir /mnt/nix
-mount -t zfs "$ZFS_DS_NIX" /mnt/nix
-
-info "Mounting '$ZFS_DS_KEEP' to /mnt/keep ..."
-mkdir /mnt/keep
-mount -t zfs "$ZFS_DS_KEEP" /mnt/keep
-
-info "Mounting '$ZFS_DS_PERSIST' to /mnt/persist ..."
-mkdir /mnt/persist
-mount -t zfs "$ZFS_DS_PERSIST" /mnt/persist
-
-info "Moving password to installation ..."
-mkdir -p /mnt/keep/etc/users
-mkdir -p /etc/users
-mv /tmp/tom /mnt/keep/etc/users/tom
-cp /mnt/keep/etc/users/tom /etc/users/tom
-
-info "Cloning NixOS configuration to /mnt/keep/etc/nixos/ ..."
-mkdir -p /mnt/keep/etc/nixos
-cd /mnt/keep/etc/nixos
-git init
-git remote add origin https://github.com/chaosinthecrd/nixos.git
-git pull origin main
-cd -
-
-info "Replacing /mnt/etc/nixos with /mnt/keep/etc/nixos ..."
-rm -rf /mnt/etc/nixos
-mkdir -p /mnt/etc
-cp -r /mnt/keep/etc/nixos/ /mnt/etc
-
-info "system linking /mnt/keep to ensure passward is captured in nix install ..."
-ln -s /mnt/keep /keep
-
-info "system linking /mnt/persist to ensure ssh is captured in nix install ..."
-ln -s /mnt/persist /persist
-
+#
+# info "Enter password for 'tom' user ..."
+# mkdir -p /mnt/keep/etc/users
+# mkdir -p /etc/users
+# mkpasswd -m sha-512 | tr -d "\n\r" > /tmp/tom
+#
+# ################################################################################
+#
+# export ZFS_POOL="rpool"
+#
+# # ephemeral datasets
+# export ZFS_LOCAL="${ZFS_POOL}/local"
+# export ZFS_DS_ROOT="${ZFS_LOCAL}/root"
+# # The below are kept on this machine.
+# export ZFS_DS_NIX="${ZFS_LOCAL}/nix"
+# export ZFS_DS_KEEP="${ZFS_LOCAL}/keep"
+#
+# # Persistent datasets which are backed up.
+# export ZFS_SAFE="${ZFS_POOL}/safe"
+# export ZFS_DS_PERSIST="${ZFS_SAFE}/persist"
+#
+# export ZFS_BLANK_SNAPSHOT="${ZFS_DS_ROOT}@blank"
+#
+# ################################################################################
+#
+# info "Running the UEFI (GPT) partitioning and formatting directions from the NixOS manual ..."
+# parted "$DISK_PATH" -- mklabel gpt
+# parted "$DISK_PATH" -- mkpart primary 512MiB 100%
+# parted "$DISK_PATH" -- mkpart ESP fat32 1MiB 512MiB
+# parted "$DISK_PATH" -- set 2 boot on
+# export DISK_PART_ROOT="${DISK_PATH}${PARTITION_PREFIX}1"
+# export DISK_PART_BOOT="${DISK_PATH}${PARTITION_PREFIX}2"
+#
+# info "Formatting boot partition ..."
+# mkfs.fat -F 32 -n boot "$DISK_PART_BOOT"
+#
+# info "Creating '$ZFS_POOL' ZFS pool for '$DISK_PART_ROOT' ..."
+# zpool create -f \
+#   -o ashift=12 \
+#   -o autotrim=on \
+#   -O canmount=off \
+#   -O mountpoint=none \
+#   -O acltype=posixacl \
+#   -O compression=lz4 \
+#   -O dnodesize=auto \
+#   -O relatime=on \
+#   -O normalization=formD \
+#   -O xattr=sa \
+#   -O encryption=aes-256-gcm \
+#   -O keylocation=prompt \
+#   -O keyformat=passphrase "$ZFS_POOL" "$DISK_PART_ROOT"
+#
+# info "Creating '$ZFS_DS_ROOT' ZFS dataset ..."
+# zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_ROOT"
+#
+# info "Creating '$ZFS_DS_NIX' ZFS dataset ..."
+# zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_NIX"
+#
+# info "Disabling access time setting for '$ZFS_DS_NIX' ZFS dataset ..."
+# zfs set atime=off "$ZFS_DS_NIX"
+#
+# info "Creating '$ZFS_DS_KEEP' ZFS dataset ..."
+# zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_KEEP"
+#
+# info "Creating '$ZFS_DS_PERSIST' ZFS dataset ..."
+# zfs create -p -o canmount=on -o mountpoint=legacy "$ZFS_DS_PERSIST"
+#
+# info "Permit ZFS auto-snapshots on ${ZFS_SAFE}/* datasets ..."
+# zfs set com.sun:auto-snapshot=true "$ZFS_DS_PERSIST"
+#
+# info "Creating '$ZFS_BLANK_SNAPSHOT' ZFS snapshot ..."
+# zfs snapshot "$ZFS_BLANK_SNAPSHOT"
+#
+# info "Mounting '$ZFS_DS_ROOT' to /mnt ..."
+# mount -t zfs "$ZFS_DS_ROOT" /mnt
+#
+# info "Mounting '$DISK_PART_BOOT' to /mnt/boot ..."
+# mkdir /mnt/boot
+# mount -t vfat "$DISK_PART_BOOT" /mnt/boot
+#
+# info "Mounting '$ZFS_DS_NIX' to /mnt/nix ..."
+# mkdir /mnt/nix
+# mount -t zfs "$ZFS_DS_NIX" /mnt/nix
+#
+# info "Mounting '$ZFS_DS_KEEP' to /mnt/keep ..."
+# mkdir /mnt/keep
+# mount -t zfs "$ZFS_DS_KEEP" /mnt/keep
+#
+# info "Mounting '$ZFS_DS_PERSIST' to /mnt/persist ..."
+# mkdir /mnt/persist
+# mount -t zfs "$ZFS_DS_PERSIST" /mnt/persist
+#
+# info "Moving password to installation ..."
+# mkdir -p /mnt/keep/etc/users
+# mkdir -p /etc/users
+# mv /tmp/tom /mnt/keep/etc/users/tom
+# cp /mnt/keep/etc/users/tom /etc/users/tom
+#
+# info "Cloning NixOS configuration to /mnt/keep/etc/nixos/ ..."
+# mkdir -p /mnt/keep/etc/nixos
+# cd /mnt/keep/etc/nixos
+# git init
+# git remote add origin https://github.com/chaosinthecrd/nixos.git
+# git pull origin main
+# cd -
+#
+# info "Replacing /mnt/etc/nixos with /mnt/keep/etc/nixos ..."
+# rm -rf /mnt/etc/nixos
+# mkdir -p /mnt/etc
+# cp -r /mnt/keep/etc/nixos/ /mnt/etc
+#
+# info "system linking /mnt/keep to ensure passward is captured in nix install ..."
+# ln -s /mnt/keep /keep
+#
+# info "system linking /mnt/persist to ensure ssh is captured in nix install ..."
+# ln -s /mnt/persist /persist
+#
 info "Installing NixOS to /mnt ..."
 NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nixos-install --no-root-passwd --flake "/mnt/etc/nixos#$HOSTNAME"
 
